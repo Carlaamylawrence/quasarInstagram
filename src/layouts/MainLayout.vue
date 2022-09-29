80a686244b37c76f8d9b23ea5207b2c911e09004
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-09-29 15:18:44 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-footer
+    class="bg-white"
+    bordered>
+      <q-tabs class="text-grey-10"
+      active-color="primary"
+      indicator-color="transparent">
+        <q-route-tab
+        to="/"
+        icon="eva-home-outline"/>
+        <q-route-tab
+        to="/camera"
+        icon="eva-camera-outline"/>
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
+<style lang="sass">
+.q-footer
+ .q-tab__icon
+  font-size:30px
+
+</style>
