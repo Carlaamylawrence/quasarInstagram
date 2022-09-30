5684a9c56d8717e38600e5fe6764f424781b7ab7
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-09-30 16:13:12 +0200

    header done

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,22 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label text-bold>carla_lawrence</q-item-label>
+          <q-item-label caption>
+            Cape Town, South Africa
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-separator />
+    </q-card>
   </q-page>
 </template>
 
