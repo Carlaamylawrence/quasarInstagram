40328150171870d5e133ccb95179920b650fd0db
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-10-27 12:27:16 +0200

    posts

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,12 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card class="card-post" flat bordered>
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post"
+      q-mb-md
+      flat
+      bordered>
       <q-item>
         <q-item-section avatar>
           <q-avatar>
@@ -9,13 +15,22 @@
         </q-item-section>
 
         <q-item-section>
-          <q-item-label text-bold>carla_lawrence</q-item-label>
+          <q-item-label class="text-bold">carla_lawrence</q-item-label>
           <q-item-label caption>
             Cape Town, South Africa
           </q-item-label>
         </q-item-section>
       </q-item>
       <q-separator />
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+      <q-card-section>
+        <div>Golden Gate Bridge </div>
+        <div class="text-caption text-grey">June 10 9:04AM</div>
+      </q-card-section>
+
     </q-card>
   </q-page>
 </template>
@@ -24,6 +39,46 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageHome'
+  name: 'PageHome',
+  data(){
+    return{
+      posts:[
+        {
+          id:1,
+          caption: 'Golden Gate Bridge',
+          date: 159177665504,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:2,
+          caption: 'Golden Gate Bridge',
+          date: 159177665504,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:3,
+          caption: 'Golden Gate Bridge',
+          date: 159177665504,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+          id:4,
+          caption: 'Golden Gate Bridge',
+          date: 159177665504,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+
+      ]
+    }
+  }
 })
 </script>
+<style lang="sass">
+  .card-post
+    -q.img
+      min-height:200px
+</style>
